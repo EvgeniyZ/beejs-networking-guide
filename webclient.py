54cf148c124a9a2852fc0f718bd556d3b52342e3
Evgenii Zakharov <e.zakharov.ext@sportradar.com>
2023-06-04 23:24:13 +0300

    refactor, improve code

--- webclient.py
+++ webclient.py
@@ -5,13 +5,24 @@ def http_request_to_bytes(http):
     return (http + '\r\n\r\n').encode("ISO-8859-1")
 
 
+def receive_response(s):
+    buffer_size = 4096
+    resp = ''
+    b = s.recv(buffer_size)
+    while len(b) != 0:
+        resp += b.decode("ISO-8859-1")
+        b = s.recv(buffer_size)
+    return resp
+
+
 print('input the desired host')
 host = input()
 s = socket.socket()
 s.connect((host, 80))
 r = "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close"
 s.sendall(http_request_to_bytes(r))
-b = s.recv(4096)
+
+response = receive_response(s)
+
 s.close()
-response = b.decode("ISO-8859-1")
 print(response)
