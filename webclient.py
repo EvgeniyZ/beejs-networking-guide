1126fa3097c29e1d6f25f7863dfa586e299bc8ad
Evgenii Zakharov <e.zakharov.ext@sportradar.com>
2023-06-04 23:13:51 +0300

    allow specify desired host

--- webclient.py
+++ webclient.py
@@ -5,8 +5,10 @@ def http_request_to_bytes(http):
     return (http + '\r\n\r\n').encode("ISO-8859-1")
 
 
+print('input the desired host')
+host = input()
 s = socket.socket()
-s.connect(("example.com", 80))
+s.connect((host, 80))
 r = "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close"
 s.sendall(http_request_to_bytes(r))
 b = s.recv(4096)
