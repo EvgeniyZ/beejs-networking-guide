dff664d5d52cf04dc6900095a58f380b16c7c138
Evgenii Zakharov <e.zakharov.ext@sportradar.com>
2023-06-18 13:04:41 +0300

    implement ip_pseudo_header func

--- tcpchecksum.py
+++ tcpchecksum.py
@@ -1,10 +1,18 @@
 def ip_to_bytes(ip: str) -> bytes:
     numbers = ip.split('.')
-    return big_endian_int_from_str(numbers[0]) + big_endian_int_from_str(numbers[1]) + big_endian_int_from_str(numbers[2]) + big_endian_int_from_str(numbers[3])
+    return big_endian_int_from_str(numbers[0]) + big_endian_int_from_str(numbers[1]) + big_endian_int_from_str(
+        numbers[2]) + big_endian_int_from_str(numbers[3])
 
 
 def big_endian_int_from_str(value):
     return int(value).to_bytes(1, byteorder="big")
 
 
-print(ip_to_bytes("192.168.1.1"))
+def ip_pseudo_header(src_ip: str, dst_ip: str, tcp_length: int) -> bytes:
+    return ip_to_bytes(src_ip) + ip_to_bytes(dst_ip) \
+        + (0).to_bytes(1, byteorder="big") \
+        + (6).to_bytes(1, byteorder="big") \
+        + tcp_length.to_bytes(4, byteorder="big")
+
+
+print(ip_pseudo_header("255.0.255.1", "127.255.0.1", 3490))
